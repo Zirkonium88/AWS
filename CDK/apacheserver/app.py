5b5a5aa02f981d10bd005b3d7b65807a1a3b92fa
Malte Walkowiak <Malte@Maltes-MBP.fritz.box>
2020-03-12 22:32:23 +0100

    Improved Apache WWW Server Example

--- CDK/apacheserver/app.py
+++ CDK/apacheserver/app.py
@@ -4,7 +4,7 @@ from aws_cdk import core
 
 from ec2_dev.ec2_dev_stack import Ec2DevStack
 
-env_EU = core.Environment(account="431892011317", region="eu-central-1")
+env_EU = core.Environment(account="XXX_XXX_XXX", region="eu-central-1")
 
 app = core.App()
 Ec2DevStack(app, "ec2Apache", env=env_EU)
--- CDK/apacheserver/userdata.sh
+++ CDK/apacheserver/userdata.sh
@@ -3,3 +3,4 @@ sudo yum update -y
 sudo yum -y install httpd php
 sudo chkconfig httpd on
 sudo service httpd start
+aws s3 cp s3://my-deployment-bucket-20201203/index.html /var/www/index.html
