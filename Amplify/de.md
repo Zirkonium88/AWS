2f55fd6ad365f457d31df807241c83302198edd7
Malte Walkowiak <Malte@Maltes-MBP.fritz.box>
2020-03-14 18:47:31 +0100

    Fixed repo problems

--- Amplify/de.md
+++
@@ -1,1 +0,0 @@
-TabNine::config 
--- Amplify/node_modules/@aws-amplify/analytics/src/Providers/index.ts
+++
@@ -1,6 +0,0 @@
-import AWSPinpointProvider from './AWSPinpointProvider';
-import AWSKinesisProvider from './AWSKinesisProvider';
-import AWSKinesisFirehoseProvider from './AWSKinesisFirehoseProvider';
-import AmazonPersonalizeProvider from './AmazonPersonalizeProvider';
-
-export { AWSPinpointProvider, AWSKinesisProvider, AWSKinesisFirehoseProvider, AmazonPersonalizeProvider };
